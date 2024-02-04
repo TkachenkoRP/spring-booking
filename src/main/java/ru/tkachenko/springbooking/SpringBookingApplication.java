1e816e74d70e861bd3cc3291060cc8bdca76e4f6
Rodion Tkachenko <tkachenko_rp@mail.ru>
2024-02-04 13:17:52 +0700

    init

---
+++ src/main/java/ru/tkachenko/springbooking/SpringBookingApplication.java
@@ -0,0 +1,13 @@
+package ru.tkachenko.springbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookingApplication.class, args);
+    }
+
+}
